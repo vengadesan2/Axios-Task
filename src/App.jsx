c0bc175c8c2a0eb3876b84460656b0c09ffa2324
vengadesan2 <vengadsanguna24@gmail.com>
2024-01-10 20:36:36 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AppRoutes from './utils/Approutes'
+import AppRoutes from './utils/AppRoutes'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 export const APP_URL= "https://659e2be747ae28b0bd3547b5.mockapi.io/blog/data"
 function App() {
